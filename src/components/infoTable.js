6003f5db7ed614a17fd929ae4a39c6bf541328c4
Quesle <eneblue@126.com>
2015-11-24 00:16:00 +0800

    components study

---
+++ src/components/infoTable.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Input} from 'react-bootstrap';
+
+
+class InfoTable extends Component {
+
+  render () {
+    return (
+        <Input
+            type="text" />
+    );
+  }
+}
+
+export default InfoTable;
