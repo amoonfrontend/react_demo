990ce2d1ea1f7115b6f8bb91aba434ed961623e6
Quesle <eneblue@126.com>
2015-11-24 00:14:05 +0800

    components study

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert('This is app.js .');
+import 'jquery';
+import 'bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import InfoTable from './src/components/infoTable.js';
+
+
+ReactDOM.render(
+  <InfoTable />,
+  document.getElementById('root')
+);
